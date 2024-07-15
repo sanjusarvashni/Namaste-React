81b899c4a95742577aff513800a850e1a7074911
sanjusarvashni <sanjusarvashni@gmail.com>
2024-07-15 11:26:12 +0530

    ep-2 assignment

--- ep-1/app.js
+++
@@ -1,6 +0,0 @@
-const parent = React.createElement("div",{id:"parent"}, React.createElement("div",{id:"child"},React.createElement("h1"),{},"Im a h1 tag"));
-
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
